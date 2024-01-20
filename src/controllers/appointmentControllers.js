ec56b5bee8a5a04186ea284abefbe9d90fbcc7fd
Lautaro Bunkowski <lautibunko@gmail.com>
2024-01-20 14:08:25 -0300

    nothing to say

--- src/controllers/appointmentControllers.js
+++ src/controllers/appointmentControllers.js
@@ -1,5 +1,5 @@
 import { Appointment, User, Service } from "../database.js";
-import { Sequelize } from "sequelize";
+import moment from "moment";
 
 export const postAppointmentController = async (
   date,
@@ -68,35 +68,59 @@ export const getAllAppointmentController = async (service, date) => {
     const allAppoinments = await Appointment.findAll({
       where: { date },
     });
-    const dayAppointmentsCalculator = (startTime, endTime, duration, allAppoinments) => {
-      const daysAppointments = []
-      let startTimeMinutes = 0;
-      let endTimeMinutes = 0;
-      let durationPerMinutes = 0;
-      startTime.split(":").forEach((e, i) => {
-        if(i === 0){
-          return startTimeMinutes = Number(e) * 60
-        }
-        startTimeMinutes += Number(e)
-      })
-      endTime.split(":").forEach((e, i) => {
-        if(i === 0){
-          return endTimeMinutes = Number(e) * 60
-        }
-        endTimeMinutes += Number(e)
-      })
-      duration.split(":").forEach((e, i) => {
-        if(i === 0){
-          return durationPerMinutes = Number(e) * 60
-        }
-        durationPerMinutes += Number(e)
-      })
-      console.log(durationPerMinutes)
-      const appointmentsPerDay = (endTimeMinutes - startTimeMinutes) / durationPerMinutes;
-      
-      return appointmentsPerDay;
-    }
-    console.log(dayAppointmentsCalculator(serviceFind.startTime, serviceFind.endTime, serviceFind.duration,allAppoinments))
+
+    console.log(moment());
+
+    // const dayAppointmentsCalculator = (
+    //   startTime,
+    //   endTime,
+    //   duration,
+    //   allAppoinments
+    // ) => {
+    //   const daysAppointments = [];
+    //   let startTimeMinutes = 0;
+    //   let endTimeMinutes = 0;
+    //   let durationPerMinutes = 0;
+    //   startTime.split(":").forEach((e, i) => {
+    //     if (i === 0) {
+    //       return (startTimeMinutes = Number(e) * 60);
+    //     }
+    //     startTimeMinutes += Number(e);
+    //   });
+    //   endTime.split(":").forEach((e, i) => {
+    //     if (i === 0) {
+    //       return (endTimeMinutes = Number(e) * 60);
+    //     }
+    //     endTimeMinutes += Number(e);
+    //   });
+    //   duration.split(":").forEach((e, i) => {
+    //     if (i === 0) {
+    //       return (durationPerMinutes = Number(e) * 60);
+    //     }
+    //     durationPerMinutes += Number(e);
+    //   });
+
+    //   const appointmentsPerDay =
+    //     (endTimeMinutes - startTimeMinutes) / durationPerMinutes;
+
+    //   let auxTime = startTimeMinutes;
+    //   for (let i = 0; i < appointmentsPerDay; i++) {
+    //     daysAppointments.push({
+    //       date: new Date(`${date}T`),
+    //       available: true,
+    //     });
+    //   }
+
+    //   return daysAppointments;
+    // };
+    // console.log(
+    //   dayAppointmentsCalculator(
+    //     serviceFind.startTime,
+    //     serviceFind.endTime,
+    //     serviceFind.duration,
+    //     allAppoinments
+    //   )
+    // );
 
     return { ...serviceFind.dataValues, appointments: allAppoinments };
   } catch (error) {
