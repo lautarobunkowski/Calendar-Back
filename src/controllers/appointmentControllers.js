d1b9960cc2bb4ccdc50f696fec305923cfbadca1
Lautaro Bunkowski <lautibunko@gmail.com>
2024-01-22 15:58:12 -0300

    22/01/24 15:58

--- src/controllers/appointmentControllers.js
+++ src/controllers/appointmentControllers.js
@@ -1,5 +1,5 @@
 import { Appointment, User, Service } from "../database.js";
-import moment from "moment";
+import dayAppointmentsCalculator from "../utils/dayAppointmentsCalculator.js";
 
 export const postAppointmentController = async (
   date,
@@ -65,64 +65,23 @@ export const getAllAppointmentController = async (service, date) => {
       };
     }
 
-    const allAppoinments = await Appointment.findAll({
-      where: { date },
+    const allAppoinments = (
+      await Appointment.findAll({
+        where: { date },
+      })
+    ).map((app) => {
+      return app.get();
     });
 
-    console.log(moment());
-
-    // const dayAppointmentsCalculator = (
-    //   startTime,
-    //   endTime,
-    //   duration,
-    //   allAppoinments
-    // ) => {
-    //   const daysAppointments = [];
-    //   let startTimeMinutes = 0;
-    //   let endTimeMinutes = 0;
-    //   let durationPerMinutes = 0;
-    //   startTime.split(":").forEach((e, i) => {
-    //     if (i === 0) {
-    //       return (startTimeMinutes = Number(e) * 60);
-    //     }
-    //     startTimeMinutes += Number(e);
-    //   });
-    //   endTime.split(":").forEach((e, i) => {
-    //     if (i === 0) {
-    //       return (endTimeMinutes = Number(e) * 60);
-    //     }
-    //     endTimeMinutes += Number(e);
-    //   });
-    //   duration.split(":").forEach((e, i) => {
-    //     if (i === 0) {
-    //       return (durationPerMinutes = Number(e) * 60);
-    //     }
-    //     durationPerMinutes += Number(e);
-    //   });
-
-    //   const appointmentsPerDay =
-    //     (endTimeMinutes - startTimeMinutes) / durationPerMinutes;
-
-    //   let auxTime = startTimeMinutes;
-    //   for (let i = 0; i < appointmentsPerDay; i++) {
-    //     daysAppointments.push({
-    //       date: new Date(`${date}T`),
-    //       available: true,
-    //     });
-    //   }
-
-    //   return daysAppointments;
-    // };
-    // console.log(
-    //   dayAppointmentsCalculator(
-    //     serviceFind.startTime,
-    //     serviceFind.endTime,
-    //     serviceFind.duration,
-    //     allAppoinments
-    //   )
-    // );
-
-    return { ...serviceFind.dataValues, appointments: allAppoinments };
+    return {
+      ...serviceFind.dataValues,
+      appointments: dayAppointmentsCalculator(
+        serviceFind.startTime,
+        serviceFind.endTime,
+        serviceFind.duration,
+        allAppoinments
+      ),
+    };
   } catch (error) {
     throw error;
   }
--- src/utils/dayAppointmentsCalculator.js
+++ src/utils/dayAppointmentsCalculator.js
@@ -1,5 +1,55 @@
-export const dayAppointmentsCalculator = (date, startTime, endTime, allAppoinments) => {
-    const daysAppointmens = []
-    
-    return daysAppointmens;
-}
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat.js";
+import duration from "dayjs/plugin/duration.js";
+
+// import tz from "dayjs/plugin/timezone.js";
+
+dayjs.extend(customParseFormat);
+dayjs.extend(duration);
+// dayjs.extend(tz);
+// console.log(dayjs.tz.guess());
+
+const dayAppointmentsCalculator = (
+  startTime,
+  endTime,
+  duration,
+  allAppoinments
+) => {
+  const durationPerMinutes = dayjs
+    .duration(dayjs(duration, "HH:mm:ss").diff(dayjs("00:00:00", "HH:mm:ss")))
+    .asMinutes();
+
+  const appointmentsPerDay =
+    dayjs
+      .duration(dayjs(endTime, "HH:mm:ss").diff(dayjs(startTime, "HH:mm:ss")))
+      .asMinutes() / durationPerMinutes;
+
+  const daysAppointments = [];
+
+  for (let i = 0; i < appointmentsPerDay; i++) {
+    const findAppointment = allAppoinments.find(
+      (app) => app.time === startTime
+    );
+
+    if (findAppointment !== undefined) {
+      daysAppointments.push({
+        time: findAppointment.time,
+        available: false,
+      });
+    } else {
+      daysAppointments.push({
+        time: startTime,
+        available: true,
+      });
+    }
+
+    startTime = dayjs(startTime, "HH:mm:ss")
+      .add(durationPerMinutes, "minute")
+      .format("HH:mm:ss");
+    console.log(startTime);
+  }
+
+  return daysAppointments;
+};
+
+export default dayAppointmentsCalculator;
